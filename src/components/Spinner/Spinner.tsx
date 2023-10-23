f9e281c65b51d411fc7146dcdb690eaca92a0ba6
Ogheneochuko Pedro <pedroogheneochuko@gmail.com>
2023-10-23 23:20:24 +0100

    feat(spinner): build out Spinner component

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,31 @@
+import { SpinnerProps } from "./Spinner.type";
+
+const Spinner = ({ size = "medium", spinSpeed = 5 }: SpinnerProps) => {
+  return (
+    <span
+      className="stc-spinner"
+      data-spin-speed={spinSpeed}
+      data-size={size}
+    >
+      <svg
+        width="16"
+        height="16"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+      >
+        <circle
+          cx="50"
+          cy="50"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="var(--stroke-width)"
+          r="45"
+          strokeDasharray="164.93361431346415 56.97787143782138"
+          transform="rotate(159.821 50 50)"
+        ></circle>
+      </svg>
+    </span>
+  );
+};
+
+export default Spinner;
