cd70f23ef1d1ec6c7d158d08fe0d2744b3c9617e
Ogheneochuko Pedro <pedroogheneochuko@gmail.com>
2023-10-23 23:18:08 +0100

    chore: Initialize storybook

---
+++ utils/getFiles.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+
+export const getFiles = (entry, extensions = [], excludeExtensions = []) => {
+  let fileNames = [];
+  const dirs = fs.readdirSync(entry);
+
+  dirs.forEach((dir) => {
+    const path = `${entry}/${dir}`;
+
+    if (fs.lstatSync(path).isDirectory()) {
+      fileNames = [
+        ...fileNames,
+        ...getFiles(path, extensions, excludeExtensions),
+      ];
+
+      return;
+    }
+
+    if (
+      !excludeExtensions.some((exclude) => dir.endsWith(exclude)) &&
+      extensions.some((ext) => dir.endsWith(ext))
+    ) {
+      fileNames.push(path);
+    }
+  });
+
+  return fileNames;
+};
+
+export const computePathFromPWD = (pwd, filePath) => {
+  const srcPath = path.join(pwd + filePath);
+
+  return srcPath;
+};
