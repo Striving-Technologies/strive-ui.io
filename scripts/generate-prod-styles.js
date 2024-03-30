6e490d190f1adb8c4a01eebcacd2aa1ffd328528
Ochuko Pedro <pedroogheneochuko@gmail.com>
2024-03-30 08:34:02 +0100

    fix: ensure parsing of style sheets during build

--- scripts/generate-prod-styles.js
+++ scripts/generate-prod-styles.js
@@ -5,19 +5,23 @@ import path from "path";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const rootPath = __dirname.split("/").slice(0, -1).join("/");
-const srcPath = rootPath + "/src";
+const rootPath = path.normalize(__dirname.split("scripts").slice(0, -1)[0]);
+const srcPath = path.join(rootPath, "src");
 
 const buildStyles = async () => {
-  const files = await globby(srcPath + "/**/*.scss", {});
+  const styleRoutes = path.join(srcPath, "**", "*.scss").replace(/\\/g, "/");
+
+  const files = await globby(styleRoutes, {});
+
+  const buildDirectory = path.join(rootPath, "dist", "components", "styles");
 
   // clean up dist/styles directory before generating new styles
-  fs.rmSync(rootPath + "/dist/components/styles", {
+  fs.rmSync(buildDirectory, {
     recursive: true,
     force: true,
   });
 
-  fs.mkdirSync(rootPath + "/dist/components/styles", { recursive: true });
+  fs.mkdirSync(buildDirectory, { recursive: true });
 
   files.forEach((file) => {
     const fileName = file.split("/").slice(-1)[0];
@@ -28,11 +32,11 @@ const buildStyles = async () => {
     const fileContent = fs.readFileSync(file, "utf-8");
 
     const result = fileContent
-      .replace(/([^0-9a-zA-Z\.#])\s+/g, "$1")
-      .replace(/\s([^0-9a-zA-Z\.#]+)/g, "$1");
+      .replace(/([^0-9a-zA-Z.#])\s+/g, "$1")
+      .replace(/\s([^0-9a-zA-Z.#]+)/g, "$1");
     // .replace(/\/\*.*?\*\//g, "");
 
-    fs.writeFileSync(rootPath + `/dist/components/styles/${fileName}`, result);
+    fs.writeFileSync(path.join(buildDirectory, fileName), result);
   });
 };
 
