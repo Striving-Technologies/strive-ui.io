a3679e3fab1db98e685d8e902d1cabde096feaa9
Ogheneochuko Pedro <ochuko.space@gmail.com>
2025-07-28 23:39:09 +0100

    feat(docs): restructure navigation of component
    - switch root folder from "Example" to "Components"

--- src/stories/Search.stories.ts
+++ src/stories/Search.stories.ts
@@ -4,7 +4,7 @@ import { Search } from "../components/Search";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: "Example/Search",
+  title: "Components/Search",
   component: Search,
   parameters: {
     layout: "centered",
