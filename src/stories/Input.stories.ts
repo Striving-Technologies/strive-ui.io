f123017327d3c692abbd6d1c38be7f26d0512a3d
Ogheneochuko Pedro <ochuko.space@gmail.com>
2025-07-28 23:39:09 +0100

    feat(docs): restructure navigation of component
    - switch root folder from "Example" to "Components"

--- src/stories/Input.stories.ts
+++ src/stories/Input.stories.ts
@@ -5,7 +5,7 @@ import { Input } from "../components/Input";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: "Example/Input",
+  title: "Components/Input",
   component: Input,
   parameters: {
     layout: "centered",
